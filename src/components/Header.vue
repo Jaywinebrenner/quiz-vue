2e17c94be56c74de86bf22a3f07ce6c00c38737d
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-10-03 09:41:11 -0700

    wire up html

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <b-nav tabs>
+    <b-nav-item disabled><b>QUIZ LORD</b></b-nav-item>
+    <b-nav-item disabled>Counter: 4/10</b-nav-item>
+  </b-nav>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="questionBoxContainer">
+  <b-jumbotron>
+    <template>{{ currentQuestion.question}}</template>
+
+
+    <hr class="my-4">
+
+    <p>
+      List of answers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button @click="next" variant="success" href="#">Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  }
+
+}
+</script>
+
+<style>
+
+</style>
